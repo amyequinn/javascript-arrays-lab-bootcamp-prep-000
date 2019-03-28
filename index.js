e4ae0a2acdaee099efe54a18aeed602e41826d8e
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 17:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var kittens = ["Milo", "Otis", "Garfield"]
 
 
-
 function destructivelyAppendKitten(Ralph) {
   kittens.push(Ralph)
+  return kittens
+}
+
+function destructivelyPrependKitten(Amy){
+  kittens.unshift(Amy)
   return kittens
 }
