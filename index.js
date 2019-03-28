c10406e608be4c2c678c83e700e2953f388626ae
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 17:06:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 
 function destructivelyAppendKitten(Ralph) {
-  kittens.push(Ralph)
+  kittens.push("Ralph")
   return kittens
 }
 
 function destructivelyPrependKitten(Amy){
-  kittens.unshift(Amy)
+  kittens.unshift("Amy")
   return kittens
 }
