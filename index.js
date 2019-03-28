019b6d4c7201aecc09ddf78dd6c879fbd2e2db60
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-28 16:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+var destructivelyPrependKitten = ["kitten"];
+console.log(kittens.concat(destructivelyPrependKitten));
+/*
+
+function destructivelyPrependKitten(name){
+  var kitten = ["kitten"];
+  console.log(kittens.concat(kitten));
+}
